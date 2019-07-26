813bd194dfb58daeb1ecf12fb8b8e630fc298977
Greg Dubrow <gdubrow@sfsu.edu>
2019-07-26 13:28:18 -0700

    purrrrrrrrr

--- purr/purr via jenny bryan.R
+++ purr/purr via jenny bryan.R
@@ -1,7 +1,9 @@
 # https://jennybc.github.io/purrr-tutorial/index.html
 
-# vecotrs and lists - ecessary because purr iterates over vectors & lists
+library(tidyverse)
+### vecoros and lists - ecessary because purr iterates over vectors & lists
 
+# vectors
 v_log <- c(TRUE, FALSE, FALSE, TRUE)
 v_log
 
@@ -18,3 +20,25 @@ is.logical(v_char)
 # test also is.numeric, is.integer, is.character
 
 v_int[0]
+
+# hieararchy of the most common atomic vector types: logical < integer < numeric < character
+# keep in mind if trying to coerce from one type to another
+
+as.logical(v_char)
+
+## lists - essentially multiple vectors in the same object. can have multiple layers.
+# when contructing by hand, each comma separates levels
+
+l_x <- list(1:3, c("four", "five"))
+
+l_y <- list(logical = TRUE, integer = 4L, double = 4 * 1.2, character = "character")
+
+is.logical(l_y)
+
+l_z <- list(letters[26:22], transcendental = c(pi, exp(1)), f = function(x) x^2)
+
+# when indexing a list single brackets return the named element or number range, i.e.
+
+l_y[2:3] # returns layers 2 & 3 of the 4 layer list
+
+l_z["transcendental"] # returns just the layer named "transcendental"
