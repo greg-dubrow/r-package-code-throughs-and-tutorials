7c88704eb60f8a323e87743e7a09ab45bf9b6624
Greg Dubrow <gdubrow@sfsu.edu>
2019-07-30 10:08:40 -0700

    purrr updates

--- purr/purr via jenny bryan.R
+++ purr/purr via jenny bryan.R
@@ -1,6 +1,8 @@
 # https://jennybc.github.io/purrr-tutorial/index.html
 
 library(tidyverse)
+library(repurrrsive)
+library(listviewer)
 ### vecoros and lists - ecessary because purr iterates over vectors & lists
 
 # vectors
@@ -42,3 +44,68 @@ l_z <- list(letters[26:22], transcendental = c(pi, exp(1)), f = function(x) x^2)
 l_y[2:3] # returns layers 2 & 3 of the 4 layer list
 
 l_z["transcendental"] # returns just the layer named "transcendental"
+
+l_z[[2]] # does same as above but using brackets
+l_z[c(TRUE, FALSE)]
+
+my_vec <- c(a = 1, b = 2, c = 3)
+my_list <- list(a = 3, b = 6, c = 9)
+my_list$a
+my_list["a"] # this and call above return 1st object by name
+my_list[1] # calls 1st object by position
+
+
+# vectorized operations
+
+n <- 5
+seq_len(n) ^ 2 # simple call that goes across vector to square 1 to n
+seq_len(n) * 3 # or mulltiples by 3
+exp(v_doub) # exponentiation of a vector
+
+# but how to do with list? PURRR!
+l_doub <- as.list(v_doub)
+l_doub
+v_doub
+map(l_doub, exp) # function is do action on right of comma to object on left of comma 
+                 # or in purrr language, map exp() over list l_doub
+
+
+## purrr lessons
+# explore example lists
+
+str(wesanderson)
+View(wesanderson)
+
+str(got_chars) # shows all list layers
+str(got_chars, list.len = 4) # limits to 1st 4
+str(got_chars[[1]], list.len = 8) # looks at 1st value, 8 layers
+
+str(got_chars, max.level = 2, list.len = 3) # three elements, 3 layers
+
+# extract elements
+
+# using map map(LIST, FUNCTION)
+
+# extract names of 1st 4 GoT characters - each call does same thing
+map(got_chars[1:4], "name")
+map(got_chars[1:4], 3)
+
+ # gets all names, in pipe
+got_chars %>%
+  map("name")
+got_chars %>%
+  map(3)
+names(got_chars)
+
+map(got_chars[1:4], "name") # returns postion and value
+map_chr(got_chars[1:4], "name") # returns just value
+
+gotnames <- map_chr(got_chars[1:4], "name") # returns just value to a chr vector
+
+# also use map_lgl(), map_int(), map_dbl()
+
+# exercise...pull id
+map_int(got_chars[1:5], "id") # can also use 2 instead of "id"
+
+# extract multiple values
+
