95f6f4546dc1f925079a52f69e1ff808dfa6f15a
greg-dubrow <dubrowg@gmail.com>
2020-04-12 15:25:43 -0700

    web scrape rvest xml2

--- web scraping with rvest and xml2.R
+++ web scraping with rvest and xml2.R
@@ -6,6 +6,7 @@ library(xml2)
 library(knitr)
 library(gt)
 library(dplyr)
+library(tidylog)
 
 hot100page <- "https://www.billboard.com/charts/hot-100"
 hot100 <- read_html(hot100page)
@@ -74,13 +75,81 @@ duration
 # peak
 # week
 
-chart_df <- as.data.frame(data.frame(rank, artist, title, trend, last, peak)) %>%
+chart_df <- as.data.frame(data.frame(rank, artist, title, 
+                                     trend, last, peak, duration, stringsAsFactors = FALSE)) %>%
   mutate(rank = as.numeric(rank)) %>%
-  mutate(last = as.numeric(last))
+  mutate(last = as.numeric(last)) %>%
+  mutate(peak = as.numeric(peak)) %>%
+  mutate(duration = as.numeric(duration)) %>%
+  mutate(difference = rank - last) %>% 
+  mutate(difference = ifelse(is.na(difference), 100 - rank, difference)) %>%
+  select(rank, artist, title, trend, last, difference, peak, duration)
   
 tibble::glimpse(chart_df)
+
+gt(chart_df)
 
 knitr::kable(chart_df %>% 
                head(10))
   
-gt(chart_df)
+
+## function to scrape Eurovision
+# https://github.com/keithmcnulty/scraping/blob/master/eurovision_scraping.R
+get_eurovision <- function(year) {
+  
+  # get url from input and read html
+  input <- paste0("https://en.wikipedia.org/wiki/Eurovision_Song_Contest_", year) 
+  chart_page <- xml2::read_html(input, fill = TRUE)
+  
+  
+  # scrape data from any sortable table
+  chart <- chart_page %>% 
+    rvest::html_nodes("#mw-content-text") %>% 
+    xml2::xml_find_all("//table[contains(@class, 'sortable')]")
+  
+  charts <- list()
+  chartvec <- vector()
+  
+  for (i in 1:length(chart)) {
+    assign(paste0("chart", i),
+           # allow for unexpected errors but warn user
+           tryCatch({rvest::html_table(chart[[i]], fill = TRUE)}, error = function (e) {print("Potential issue discovered in this year!")})
+    )
+    
+    
+    charts[[i]] <- get(paste0("chart", i))
+    # only include tables that have Points
+    chartvec[i] <- sum(grepl("Points", colnames(get(paste0("chart", i))))) == 1 & sum(grepl("Category|Venue|Broadcaster", colnames(get(paste0("chart", i))))) == 0 
+  }
+  
+  results_charts <- charts[chartvec]
+  
+  # account for move to semifinals and qualifying rounds
+  if (year < 1956) {
+    stop("Contest was not held before 1956!")
+  } else if (year == 1956) {
+    stop("Contest was held in 1956 but no points were awarded!")
+  } else if (year %in% c(1957:1995)) {
+    results_charts[[1]] %>% 
+      dplyr::arrange(Place) %>% 
+      dplyr::select(-Draw)
+  } else if (year == 1996) {
+    results_charts[[2]] %>% 
+      dplyr::arrange(Place) %>% 
+      dplyr::select(-Draw)
+  } else if (year %in% 1997:2003) {
+    results_charts[[1]] %>% 
+      dplyr::arrange(Place) %>% 
+      dplyr::select(-Draw)
+  } else if (year %in% 2004:2007) {
+    results_charts[[2]] %>% 
+      dplyr::arrange(Place) %>% 
+      dplyr::select(-Draw)
+  } else {
+    results_charts[[3]] %>% 
+      dplyr::arrange(Place) %>% 
+      dplyr::select(-Draw)
+  }
+}
+
+get_eurovision(1965)
